f18386f500e017cd4559eb8c9294dbb1b20b58df
JSEvgeny <linc-sk8@mail.ru>
2022-04-02 05:44:03 +0300

    Add websocket dependency and generate gateway boilerplate

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -1,5 +1,4 @@
 import { Module } from '@nestjs/common';
-
 import { WebsocketModule } from './websocket/websocket.module';
 
 @Module({
--- packages/server/src/websocket/websocket.module.ts
+++ packages/server/src/websocket/websocket.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { WebsocketGateway } from './websocket.gateway';
 
-@Module({})
+@Module({ providers: [WebsocketGateway] })
 export class WebsocketModule {}
