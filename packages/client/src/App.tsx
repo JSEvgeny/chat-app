bcc3f7eb5bc074a19b04d8d13464a463fefb649e
JSEvgeny <linc-sk8@mail.ru>
2022-04-02 06:12:03 +0300

    Add styled-components to client

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,41 +1,20 @@
 import { useState } from "react";
+import styled from "styled-components";
+
+const Container = styled.div``;
+
+const Title = styled.h1`
+  color: red;
+  font-size: 24px;
+`;
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {" | "}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
-    </div>
+    <Container>
+      <Title>Welcome to the chat</Title>
+    </Container>
   );
 }
 
