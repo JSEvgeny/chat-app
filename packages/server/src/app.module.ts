b586dcc83df8468363951ab183e0cecc544a923a
JSEvgeny <linc-sk8@mail.ru>
2022-04-03 17:50:03 +0300

    Add build script to root. Serve static react from nest server

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -1,7 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ServeStaticModule } from '@nestjs/serve-static';
 import { WebsocketModule } from './websocket/websocket.module';
+import { join } from 'path';
 
 @Module({
-  imports: [WebsocketModule],
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '../../client', 'dist'),
+    }),
+    WebsocketModule,
+  ],
 })
 export class AppModule {}
